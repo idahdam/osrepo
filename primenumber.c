cd5f05de9a7c282cbbdf81cb9a59efb2b9fc5169
hadi <40880027+idahdam@users.noreply.github.com>
2021-03-16 19:39:45 +0700

    Add files via upload

---
+++ primenumber.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 100000, a, m = 200000, b=0, c = 0,x=0;
+  
+    for ( x = n; x<m ;x++)
+    {
+        for(a = 1; a<=x ; a++)
+        {
+            if (x%a == 0)
+            {
+                c++;
+            }
+        }
+        if (c == 2)
+        {
+            b++;
+        }
+        c=0;
+    }
+    printf("\nAmount of prime number :%d", b);
+    return 0;
+}
