b3da354bac6300242538d4e06b982dd692c4b041
hadi <40880027+idahdam@users.noreply.github.com>
2021-03-17 17:04:46 +0700

    Add files via upload

--- primenumbermod.c
+++ primenumbermod.c
@@ -4,23 +4,32 @@
 
 void* numberOne = "numberOne";
 void* numberTwo = "numberTwo";
+void* numberThree = "numberThree";
+void* numberFour = "numberFour";
+
 //unsynchronized
 
 void *prime_thread();
 
 int main()
 {
-    pthread_t thread1, thread2; //ganti thread 
-    int stat1, stat2;
+    pthread_t thread1, thread2, thread3, thread4; //ganti thread 
+    int stat1, stat2, stat3, stat4;
     		
     stat1 = pthread_create(&thread1, NULL, prime_thread, (void*) numberOne);
     stat2 = pthread_create(&thread2, NULL, prime_thread, (void*) numberTwo);
+    stat3 = pthread_create(&thread2, NULL, prime_thread, (void*) numberThree);
+    stat4 = pthread_create(&thread2, NULL, prime_thread, (void*) numberFour);
     
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
+    pthread_join(thread4, NULL);
     
     printf("Thread no 1 returns: %d\n", stat1);
     printf("Thread no 2 returns: %d\n", stat2);
+    printf("Thread no 3 returns: %d\n", stat3);
+    printf("Thread no 4 returns: %d\n", stat4);
     
     return 0;
 }
@@ -29,7 +38,7 @@ void *prime_thread(void* message){
     char* urutan;
     urutan = (char*) message;
 
-	int n = 100000, a, m = 200000, b=0, c = 0,x=0;
+	int n = 1000000, a, m = 2000000, b=0, c = 0,x=0;
     for ( x = n; x<m ;x++)
     {
         for(a = 1; a<=x ; a++)
